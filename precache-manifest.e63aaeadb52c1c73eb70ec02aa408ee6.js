10b60ed69ec55fc51a09d1eea40fb758313c3f84
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-11-10 12:23:47 +0100

    Updates

---
+++ precache-manifest.e63aaeadb52c1c73eb70ec02aa408ee6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3f2eb815eaf1851c3efbd0838907f8",
+    "url": "/microverse_js_bookstore/index.html"
+  },
+  {
+    "revision": "e57d0cfaa09e315c7ec3",
+    "url": "/microverse_js_bookstore/static/css/main.f9382655.chunk.css"
+  },
+  {
+    "revision": "0bafa5ec8a956a287f05",
+    "url": "/microverse_js_bookstore/static/js/2.8e3a0073.chunk.js"
+  },
+  {
+    "revision": "672521cc2de9b0123eec193d57c41043",
+    "url": "/microverse_js_bookstore/static/js/2.8e3a0073.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e57d0cfaa09e315c7ec3",
+    "url": "/microverse_js_bookstore/static/js/main.b5722c1c.chunk.js"
+  },
+  {
+    "revision": "1a684dd5571124f8dd12",
+    "url": "/microverse_js_bookstore/static/js/runtime-main.ba377695.js"
+  }
+]);
